6328c14266ac2ddf56d1fca72dcadc234c2c3bca
ueirt365 <nguyendinhtrieu365@gmail.com>
2022-05-28 20:00:33 +0700

    10 - Using React Context

--- 10_handling-side-efects-using-reducers-using-context-api/src/components/Login/Login.js
+++ 10_handling-side-efects-using-reducers-using-context-api/src/components/Login/Login.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect, useReducer } from 'react';
-
+import React, { useReducer } from 'react';
+import Button from '../UI/Button/Button';
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
+
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
