bbc8fb1b29c322f198f2413869bc5e3990e475cb
ueirt365 <nguyendinhtrieu365@gmail.com>
2022-05-29 08:27:20 +0700

    14 - Using async await

--- 14_sending-http-requests/src/App.js
+++ 14_sending-http-requests/src/App.js
@@ -5,20 +5,18 @@ import MoviesList from "./components/MoviesList";
 function App() {
   const [movies, setMovies] = useState([]);
 
-  const fetchMovies = () => {
-    fetch("https://swapi.dev/api/films")
-      .then((response) => response.json())
-      .then((moviesData) => {
-        const transformMovies = moviesData.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformMovies);
-      });
+  const fetchMovies = async () => {
+    const response = await fetch("https://swapi.dev/api/films");
+    const moviesData = await response.json();
+    const transformMovies = moviesData.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformMovies);
   };
 
   return (
