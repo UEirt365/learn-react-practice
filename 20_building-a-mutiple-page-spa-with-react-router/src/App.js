81d95cea08460b3a511af72ca9692209f69db149
ueirt365 <nguyendinhtrieu365@gmail.com>
2022-05-31 06:54:21 +0700

    20 - Using NavLink

--- 20_building-a-mutiple-page-spa-with-react-router/src/App.js
+++ 20_building-a-mutiple-page-spa-with-react-router/src/App.js
@@ -1,18 +1,20 @@
 import { Route } from "react-router-dom";
-import Header from "./components/Header";
+import MainHeader from "./components/MainHeader";
 import Products from "./pages/Products";
 import Welcome from "./pages/Welcome";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
