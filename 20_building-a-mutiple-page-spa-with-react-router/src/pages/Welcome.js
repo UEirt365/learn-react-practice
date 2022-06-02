ed073a241136c701bc1764103db956be865f22ec
ueirt365 <nguyendinhtrieu365@gmail.com>
2022-06-02 21:57:18 +0700

    20 - Nested toute & redirect user

--- 20_building-a-mutiple-page-spa-with-react-router/src/pages/Welcome.js
+++ 20_building-a-mutiple-page-spa-with-react-router/src/pages/Welcome.js
@@ -1,5 +1,14 @@
+import { Route } from "react-router-dom";
+
 const Welcome = () => {
-  return <h1>The Welcome page</h1>;
+  return (
+    <section>
+      <h1>The Welcome page</h1>
+      <Route path="/welcome/hello" exact>
+        <h1>Hello!!!</h1>
+      </Route>
+    </section>
+  );
 };
 
 export default Welcome;
