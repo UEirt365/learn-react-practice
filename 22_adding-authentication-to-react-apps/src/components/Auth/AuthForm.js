c0d24a5d5a75c5b9d3c6f356b2df3dd1794620b6
ueirt365 <nguyendinhtrieu365@gmail.com>
2022-06-07 07:10:53 +0700

    22 - Add user sign in

--- 22_adding-authentication-to-react-apps/src/components/Auth/AuthForm.js
+++ 22_adding-authentication-to-react-apps/src/components/Auth/AuthForm.js
@@ -1,24 +1,59 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
   const [isLogin, setIsLogin] = useState(true);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (isLogin) {
+    } else {
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBiliE6XEMZGvuoldYCJshvvycYBjDiQJ4",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      ).then((res) => {
+        if (res.ok) {
+          console.log("success!");
+        } else {
+          res.json().then((data) => {
+            if (data && data.error && data.error.message) {
+              alert(data.error.message);
+            }
+          });
+        }
+      });
+    }
+  };
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required />
+          <input type="email" id="email" required ref={emailRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="password">Your Password</label>
-          <input type="password" id="password" required />
+          <input type="password" id="password" required ref={passwordRef} />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? "Login" : "Create Account"}</button>
