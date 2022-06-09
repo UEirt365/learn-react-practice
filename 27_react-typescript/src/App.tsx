763ff185284e6f2d16aa91287ccc92af5b749268
ueirt365 <nguyendinhtrieu365@gmail.com>
2022-06-09 22:33:01 +0700

    27 - Add Todo component

--- 27_react-typescript/src/App.tsx
+++ 27_react-typescript/src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Todos from "./components/Todos";
+import Todo from "./models/Todo";
 
 function App() {
+  const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
   return (
-    <div >
+    <div>
+      <Todos items={todos} />
     </div>
   );
 }
