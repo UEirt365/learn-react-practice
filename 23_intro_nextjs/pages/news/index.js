e0b3aa99a99a1c88474c905096d87c02b99a7d2e
ueirt365 <nguyendinhtrieu365@gmail.com>
2022-06-15 07:21:34 +0700

    23 - Dynamic params & Link

--- 23_intro_nextjs/pages/news/index.js
+++ 23_intro_nextjs/pages/news/index.js
@@ -1,5 +1,20 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 const News = () => {
-  return <h1>The NewsPage</h1>;
+  return (
+    <Fragment>
+      <h1>The NewsPage</h1>
+      <ul>
+        <li>
+          <Link href="/news/hello1">hello1</Link>
+        </li>
+        <li>
+          <Link href="/news/hello2">hello2</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default News;
