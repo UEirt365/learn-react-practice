2e1fbc59a96eead4e47a6981e7b8f7ea4e636837
ueirt365 <nguyendinhtrieu365@gmail.com>
2022-06-15 07:47:25 +0700

    23 - Outputing list of meetups

---
+++ 23_meetup-project/pages/index.js
@@ -0,0 +1,24 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "Meetup 1",
+    image:
+      "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+    address: "Ha Noi 1",
+  },
+  {
+    id: "m2",
+    title: "Meetup 2",
+    image:
+      "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+    address: "Ha Noi 2",
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
+
+export default HomePage;
