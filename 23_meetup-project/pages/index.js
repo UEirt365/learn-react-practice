4f0237aa8ac6c6a34ca741e561237770f92d4cf0
ueirt365 <nguyendinhtrieu365@gmail.com>
2022-06-15 20:30:13 +0700

    23 - Using getServerSideProps

--- 23_meetup-project/pages/index.js
+++ 23_meetup-project/pages/index.js
@@ -21,12 +21,24 @@ const HomePage = (props) => {
   return <MeetupList meetups={props.meetups} />;
 };
 
-export async function getStaticProps() {
+export async function getServerSideProps(context) {
+  const req = context.req;
+  const res = context.res;
+
   return {
     props: {
       meetups: DUMMY_MEETUPS,
     },
   };
 }
+
+// export async function getStaticProps() {
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//     revalidate: 1,
+//   };
+// }
 
 export default HomePage;
