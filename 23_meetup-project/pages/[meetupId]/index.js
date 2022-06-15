a06b54138d56852e13d010e5687c15ab1bda8133
ueirt365 <nguyendinhtrieu365@gmail.com>
2022-06-15 20:53:34 +0700

    23 - Working with params for SSG data fetching & using getStaticPaths

--- 23_meetup-project/pages/[meetupId]/index.js
+++ 23_meetup-project/pages/[meetupId]/index.js
@@ -1,14 +1,49 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
-      image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-      title="title"
-      address="address"
-      description="description"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
+}
+
+export function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+        title: `Title ${meetupId}`,
+        address: `Address ${meetupId}`,
+        description: `Description ${meetupId}`,
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
