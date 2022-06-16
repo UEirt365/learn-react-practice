ba2905ba07385b790f19a34097a27fa5ae70842b
ueirt365 <nguyendinhtrieu365@gmail.com>
2022-06-16 07:45:03 +0700

    23 - Create meetup API & connect mongodb

--- 23_meetup-project/pages/new-meetup/index.js
+++ 23_meetup-project/pages/new-meetup/index.js
@@ -1,7 +1,25 @@
+import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-  return <NewMeetupForm />;
+  const router = useRouter();
+
+  async function addMeetupHandler(meetupData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await response.json();
+
+    console.log(result);
+
+    router.push("/");
+  }
+
+  return <NewMeetupForm addMeetupHandler={addMeetupHandler} />;
 }
 
 export default NewMeetupPage;
